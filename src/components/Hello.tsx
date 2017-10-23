0271b58466869fe67567a04be3b855fa07ff82b5
Mike Walsh <mikewalsh2@gmail.com>
2017-10-23 23:48:59 +0100

    Reformatting and page title changes.

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -3,6 +3,8 @@ import * as React from "react";
 export interface HelloProps {
     compiler: string;
     framework: string;
-}
+};
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+export const Hello = (props: HelloProps) => {
+    return <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+};
